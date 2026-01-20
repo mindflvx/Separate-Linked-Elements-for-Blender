85cd9dd26afb832ffa8647dbe8be0fa573b67718
mindflvx <jannesn@gmail.com>
2026-01-20 14:40:43 +0200

    Update operator.py

--- operator.py
+++ operator.py
@@ -1,30 +1,3 @@
-# SEPARATE LINKED ELEMENENTS
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>. 
-
-bl_info = {
-    "name": "Separate Linked Elements",
-    "author": "mindflux",
-    "version": (1, 0, 0),
-    "blender": (5, 0, 0),
-    "location": "Object menu > Separate Linked Elements",
-    "description": "Separate linked elemenents of an object and places them into a new collection.",
-    "warning": "",
-    "wiki_url": "",
-    "category": "Camera",
-}
-
 import bpy
 from bpy.types import Operator
 from bpy.props import StringProperty, BoolProperty
@@ -129,3 +102,4 @@ def register():
 
 def unregister():
     bpy.utils.unregister_class(OBJECT_OT_separate_linked_elements)
+
