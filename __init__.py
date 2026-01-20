343ed9e7910796dc9ea2506fdbc65bd459c897d5
mindflvx <jannesn@gmail.com>
2026-01-20 14:40:13 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,27 @@
+# SEPARATE LINKED ELEMENENTS
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>. 
+
 bl_info = {
     "name": "Separate Linked Elements",
-    "author": "Your Name",
+    "author": "mindflux",
     "version": (1, 0, 0),
     "blender": (5, 0, 0),
-    "location": "View3D > Object > Separate Linked Elements",
-    "description": "Separates linked elements and places them in a new collection",
+    "location": "Object menu > Separate Linked Elements",
+    "description": "Separate linked elemenents of an object and places them into a new collection.",
+    "warning": "",
+    "wiki_url": "",
     "category": "Object",
 }
 
